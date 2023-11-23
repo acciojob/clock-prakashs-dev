8e269881905b29d6acdfb226c84c27c7a67ea96d
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-23 18:25:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+(function getTimeDate() {
+  const time = document.getElementById("timer");
+  let date = new Date();
+  time.innerText = date.toLocaleDateString() + ", " + date.toLocaleTimeString();
+  setTimeout(getTimeDate, 1000);
+})();
